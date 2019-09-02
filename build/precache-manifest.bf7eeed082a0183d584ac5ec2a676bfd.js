2bd274d680310fa30de0d4584545167612859f1b
Ricardo Dias <slowmotion@outlook.com>
2019-09-02 15:44:29 +0100

    add som npm scripts

---
+++ build/precache-manifest.bf7eeed082a0183d584ac5ec2a676bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dc54498385db2bb6a1c2b4c105a588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c4f0abb2051540c507",
+    "url": "/static/css/main.243fc306.chunk.css"
+  },
+  {
+    "revision": "e531ad2ce5ea044f30ae",
+    "url": "/static/js/2.19635263.chunk.js"
+  },
+  {
+    "revision": "25c4f0abb2051540c507",
+    "url": "/static/js/main.6e2ffe52.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
